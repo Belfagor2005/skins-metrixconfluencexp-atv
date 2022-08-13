8e28cac0c6204886ecda991bd189bf8a504de99b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-08-13 18:32:24 +0200

    minor fix

--- usr/lib/enigma2/python/Components/Renderer/xvolumeText.py
+++ usr/lib/enigma2/python/Components/Renderer/xvolumeText.py
@@ -26,7 +26,7 @@ from Components.VariableText import VariableText
 from enigma import eLabel, eDVBVolumecontrol, eTimer, ePoint
 from Components.Renderer.Renderer import Renderer
 
-class xvolumeText(Renderer, VariableText):
+class xVolumeText(Renderer, VariableText):
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
